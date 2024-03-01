5b2e4e98da50399c4d77c2a4e9f399081ec9f355
Carlos Vigil <18217279+carlosvigil@users.noreply.github.com>
2024-03-01 10:52:42 -0500

    Add waybar to backup script

--- backup-confs.sh
+++ backup-confs.sh
@@ -3,4 +3,4 @@
 config=/home/c/.config
 
 cp --interactive --verbose --recursive --update --target-directory=/home/c/repos/dot-files/ \
-/etc/nixos /etc/keyd $config/hypr $config/kanshi $config/kitty $config/foot
+/etc/nixos /etc/keyd $config/hypr $config/kanshi $config/kitty $config/foot $config/waybar
